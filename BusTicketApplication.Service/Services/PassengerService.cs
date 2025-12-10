e20397652fcd7b1aec987cd312484d306d00a58c
Ktlyctrk <ktlyyctrk@gmail.com>
2025-12-11 00:33:04 +0300

    Add service classes for Passenger, Route, Ticket, and Trip
    Introduce PassengerService, RouteService, TicketService, and TripService classes in the Service.Services namespace. Each class inherits from the generic Service base class, implements its respective service interface, and is wired for dependency injection with repository, unit of work, and AutoMapper dependencies. No additional business logic is included at this stage.

---
+++ BusTicketApplication.Service/Services/PassengerService.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using BusTicketApplication.Core.Models.Dtos;
+using BusTicketApplication.Core.Models.Entities;
+using BusTicketApplication.Core.RepositoryInterfaces;
+using BusTicketApplication.Core.ServiceInterfaces;
+using BusTicketApplication.Core.UnitOfWorkInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusTicketApplication.Service.Services
+{
+    public class PassengerService(IPassengerRepository passengerRepository,IUnitOfWork unitOfWork, IMapper mapper):Service<Passenger,PassengerDto>(passengerRepository, unitOfWork, mapper), IPassengerService
+    {
+    }
+}
---
+++ BusTicketApplication.Service/Services/TripService.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using BusTicketApplication.Core.Models.Dtos;
+using BusTicketApplication.Core.Models.Entities;
+using BusTicketApplication.Core.RepositoryInterfaces;
+using BusTicketApplication.Core.ServiceInterfaces;
+using BusTicketApplication.Core.UnitOfWorkInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusTicketApplication.Service.Services
+{
+    public class TripService(ITripRepository tripRepository, IUnitOfWork unitOfWork, IMapper mapper) : Service<Trip, TripDto>(tripRepository, unitOfWork, mapper), ITripService
+    {
+    }
+}
